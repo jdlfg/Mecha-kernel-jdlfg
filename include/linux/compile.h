ccf0b9050324b569216c438c23190add6041af44
GJDLFG <gjdlfg@ubuntu.(none)>
2011-04-17 10:51:29 -0400

    refined voltage increments

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 81 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#81 PREEMPT Sun Apr 17 10:37:27 EDT 2011"
+#define LINUX_COMPILE_TIME "10:37:27"
+#define LINUX_COMPILE_BY "gjdlfg"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN
+#define LINUX_COMPILER "gcc version 4.4.0 (GCC) "
